ea0fcb84abe8295c4e674950aa22e943e6c4aed3
Zlatan Vasović <zlatanvasovic@gmail.com>
2013-11-02 16:19:44 +0100

    Bug +note

--- XDroid.py
+++ XDroid.py
@@ -48,7 +48,7 @@ while 1:
     irc.send("PRIVMSG " + channel + " :The value of pi is 3.14159265359.\n")
 
   if msg.find("+note") != -1:
-    irc.send("NOTICE " + channel + " :Something failed. Sorry.\n")
+    irc.send("NOTICE " + channel + " :An error occurred. Sorry.\n")
 
   if msg.find("+tux") != -1:
     irc.send("PRIVMSG " + channel + " :    .--.\n")
